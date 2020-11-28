15e460b2459178e9618f6cadbca4d7e4bc2231b4
Nicolas Vergez <nicolas.vergez@epitech.eu>
2020-11-28 07:49:27 +0100

    ADD checkbox for choosing display widget

--- app/src/components/Home/Home.js
+++ app/src/components/Home/Home.js
@@ -1,8 +1,14 @@
-import React,{ useEffect, Component } from 'react';
+import React,{ Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import { getUser } from '../../api/users/users';
 import { getUrlTwitch } from '../../api/oauth/Twitch';
 import TwitterLogin from 'react-twitter-login';
+
+import Checkbox from '@material-ui/core/Checkbox';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormLabel from '@material-ui/core/FormLabel';
+import FormControl from '@material-ui/core/FormControl';
 
 import FollowerWidgetContainer from '../Widget/FollowerTwitterWidget/NumberWidgetContainer';
 import TweetWidgetContainer from '../Widget/TweetWidget/TweetWidgetContainer';
@@ -18,7 +24,21 @@ class Home extends Component {
 
     constructor() {
         super();
+
+        this.state = {
+            twitterFollowers: false,
+            makeATweet: false,
+            LastDm: false,
+            twitchViews: false,
+            firstStream: false,
+            searchStream: false,
+            twitterAuth: localStorage.getItem("TOKEN_TWITTER") ? true : false
+        }
     }
+
+    handleChange = (event) => {
+        this.setState({ ...this.state, [event.target.name]: event.target.checked });
+      };
 
     componentDidMount() {
         var res = getUser(localStorage.getItem("ID_TOKEN"));
@@ -45,89 +65,73 @@ class Home extends Component {
     authTwitterHandler = (err, data) => {
         localStorage.setItem("TOKEN_TWITTER", data.oauth_token)
         localStorage.setItem("SECRET_TWITTER", data.oauth_token_secret)
+        this.setState({ ...this.state, twitterAuth: true })
     };
 
     render() {
         return(
             <div className="mt-2">
+                {this.state.twitterAuth ? <FormControl component="fieldset">
+                    <FormLabel component="legend">Choose your twitter's widgets</FormLabel>
+                    <FormGroup>
+                        <FormControlLabel
+                            control={<Checkbox checked={this.state.twitterFollowers} onChange={this.handleChange} name="twitterFollowers" />}
+                            label="Twitter followers counter"
+                        />
+                        <FormControlLabel
+                            control={<Checkbox checked={this.state.makeATweet} onChange={this.handleChange} name="makeATweet" />}
+                            label="Make a tweet"
+                        />
+                        <FormControlLabel
+                            control={<Checkbox checked={this.state.LastDm} onChange={this.handleChange} name="LastDm" />}
+                            label="See your last DM"
+                        />
+                    </FormGroup>
+                </FormControl> : null}
+                {localStorage.getItem("TOKEN_TWITCH") ? <FormControl component="fieldset">
+                    <FormLabel component="legend">Choose your twitch's widgets</FormLabel>
+                    <FormGroup>
+                        <FormControlLabel
+                            control={<Checkbox checked={this.state.twitchViews} onChange={this.handleChange} name="twitchViews" />}
+                            label="Get the number of views on your channel"
+                        />
+                        <FormControlLabel
+                            control={<Checkbox checked={this.state.firstStream} onChange={this.handleChange} name="firstStream" />}
+                            label="Get the first stream"
+                        />
+                        <FormControlLabel
+                            control={<Checkbox checked={this.state.searchStream} onChange={this.handleChange} name="searchStream" />}
+                            label="Get a stream by username"
+                        />
+                    </FormGroup>
+                </FormControl> : null}
                 <div className="Home">
                     <div className="ServiceTwitch">
-                        <div className="loginTwitch">
+                        {localStorage.getItem("TOKEN_TWITCH") == null ? <div className="loginTwitch">
                             <a href={getUrlTwitch()}>Connect my Twitch account</a>
-                        </div>
+                        </div> : null}
                     </div>
                     <div className="ServiceTwitter">
-                        <div className="loginTwitter">
+                        {localStorage.getItem("TOKEN_TWITTER") == null ? <div className="loginTwitter">
                             <TwitterLogin
                                 authCallback={this.authTwitterHandler}
                                 consumerKey="alNDFDcvK4g3lW965J1udkqmA"
                                 consumerSecret="WoUhrTEa68fw1i5NEjrnPOZHYWHmxvc9WcHKGgvRnCrhewqpP3"
                             />
-                        </div>
-                        <div className="Dashboard">
-                            <FollowerWidgetContainer heading="Twitter followers"/>
-                            <TweetWidgetContainer heading="Tweet here !"/>
-                            <LastDmWidgetContainer heading="Here, your last direct Message !"/>
-                            <ViewsTwitchWidgetContainer heading="Your views number on Twitch"/>
-                            <BestStreamWidgetContainer heading="First stream in viewers"/>
-                            <StreamWidgetContainer heading="Search a streamer"/>
-                        </div>
+                        </div> : null}
+                    </div>
+                    <div className="Dashboard">
+                        {this.state.twitterFollowers ? <FollowerWidgetContainer heading="Twitter followers"/> : null}
+                        {this.state.makeATweet ? <TweetWidgetContainer heading="Tweet here !"/> : null}
+                        {this.state.LastDm ? <LastDmWidgetContainer heading="Here, your last direct Message !"/> : null}
+                        {this.state.twitchViews ? <ViewsTwitchWidgetContainer heading="Your views number on Twitch"/> : null}
+                        {this.state.firstStream ? <BestStreamWidgetContainer heading="First stream in viewers"/> : null}
+                        {this.state.searchStream ? <StreamWidgetContainer heading="Search a streamer"/> : null}
                     </div>
                 </div>
             </div>
         )
     }
 }
-
-/*
-function Home(props) {
-    useEffect(() => {
-        var res = getUser(localStorage.getItem("ID_TOKEN"));
-        res.then((value) => {
-            if (!value.id) {
-                redirectToLogin()
-            }
-        })
-      })
-
-    function redirectToLogin() {
-    props.history.push('/login');
-    }
-
-    const authTwitterHandler = (err, data) => {
-        localStorage.setItem("TOKEN_TWITTER", data.oauth_token)
-        localStorage.setItem("SECRET_TWITTER", data.oauth_token_secret)
-    };
-
-    return(
-        <div className="mt-2">
-            <div className="Home">
-                <div className="ServiceTwitch">
-                    <div className="loginTwitch">
-                        <a href={getUrlTwitch()}>Connect my Twitch account</a>
-                    </div>
-                </div>
-                <div className="ServiceTwitter">
-                    <div className="loginTwitter">
-                        <TwitterLogin
-                            authCallback={authTwitterHandler}
-                            consumerKey="alNDFDcvK4g3lW965J1udkqmA"
-                            consumerSecret="WoUhrTEa68fw1i5NEjrnPOZHYWHmxvc9WcHKGgvRnCrhewqpP3"
-                        />
-                    </div>
-                    <div className="Dashboard">
-                        <FollowerWidgetContainer heading="Twitter followers"/>
-                        <TweetWidgetContainer heading="Tweet here !"/>
-                        <LastDmWidgetContainer heading="Here, your last direct Message !"/>
-                        <ViewsTwitchWidgetContainer heading="Your views number on Twitch"/>
-                        <BestStreamWidgetContainer heading="First stream in viewers"/>
-                        <StreamWidgetContainer heading="Search a streamer"/>
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
-}
-*/
 
 export default withRouter(Home);
